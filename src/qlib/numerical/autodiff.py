54d92e6ecae41eb41f057901a6eb6cf971386fc4
Philippe LE <le.philippe.duc.tai@gmail.com>
2024-07-08 17:48:01 +0200

    Light refactorings.

--- src/qlib/numerical/autodiff.py
+++ src/qlib/numerical/autodiff.py
@@ -10,6 +10,14 @@ def make_gradient(grad: float, id: str | list):
     return xa.DataArray([grad], coords={"a": list(id)})
 
 
+def align_arrays(left: xa.DataArray, right: xa.DataArray) -> xa.DataArray:
+    return xa.align(left, right, join="outer", fill_value=0.0)
+
+
+def extract_id(grad: xa.DataArray):
+    return grad.coords["a"].values.tolist()
+
+
 class Variable:
     def __init__(self, id: str, f: float = 1.0, grad: float = 1.0):
         self.id = [id] if isinstance(id, str) else id
@@ -26,11 +34,9 @@ class Variable:
             id = self.id
         else:
             f = self.f + other.f
-            grad_aligned, other_grad_ligned = xa.align(
-                self.grad, other.grad, join="outer", fill_value=0.0
-            )
-            grad = grad_aligned + other_grad_ligned
-            id = grad.coords["a"].values.tolist()
+            df, dg = align_arrays(self.grad, other.grad)
+            grad = df + dg
+            id = extract_id(grad)
         return Variable(id, f, grad)
 
     def __radd__(self, other: Self | float):
@@ -43,11 +49,9 @@ class Variable:
             id = self.id
         else:
             f = self.f * other.f
-            grad_aligned, other_grad_ligned = xa.align(
-                self.grad, other.grad, join="outer", fill_value=0.0
-            )
-            grad = grad_aligned * other.f + other_grad_ligned * self.f
-            id = grad.coords["a"].values.tolist()
+            df, dg = align_arrays(self.grad, other.grad)
+            grad = df * other.f + dg * self.f
+            id = extract_id(grad)
         return Variable(id, f, grad)
 
     def __rmul__(self, other: Self | float):
@@ -60,15 +64,15 @@ class Variable:
             id = self.id
         else:
             f = self.f / other.f
-            df, dg = xa.align(self.grad, other.grad, join="outer", fill_value=0.0)
+            df, dg = align_arrays(self.grad, other.f)
             grad = (df * other.f - dg * self.f) / (other.f**2)
-            id = grad.coords["a"].values.tolist()
+            id = extract_id(grad)
         return Variable(id, f, grad)
 
     def __rtruediv__(self, other: Self | float):
         if isinstance(other, float) or isinstance(other, int):
             f = other / self.f
-            grad = self.grad / self.f**2
+            grad = -other * self.grad / self.f**2
             id = self.id
             return Variable(id, f, grad)
         return other.__truediv__(self)
@@ -79,34 +83,38 @@ class Variable:
     def __rsub__(self, other: Self | float | int):
         return other + (-1) * self
 
-    def exp(self):
+    def exp(self) -> Self:
         f = np.exp(self.f)
         grad = np.exp(self.f) * self.grad
         return Variable(self.id, f, grad)
 
-    def sin(self):
+    def sin(self) -> Self:
         f = np.sin(self.f)
         grad = np.cos(self.f) * self.grad
         return Variable(self.id, f, grad)
 
-    def __pow__(self, n: float):
+    def __pow__(self, n: float) -> Self:
         f = self.f**n
         grad = n * self.f ** (n - 1) * self.grad
         return Variable(self.id, f, grad)
 
-    def log(self):
+    def log(self) -> Self:
         f = np.log(self.f)
         grad = self.grad / self.f
         return Variable(self.id, f, grad)
 
-    def ndtr(self):
+    def ndtr(self) -> Self:
         f = ndtr(self.f)
         grad = norm.pdf(self.f) * self.grad
         return Variable(self.id, f, grad)
 
 
 def main():
-    pass
+    x = Variable("x", f=3)
+    y = Variable("y", f=-1)
+    z = Variable("z", f=2)
+    f = (x * y) * (y * z).sin()
+    print(f)
 
 
 if __name__ == "__main__":

97d00a09490f39929be090d11176147b00147e43
Philippe LE <le.philippe.duc.tai@gmail.com>
2024-07-08 15:16:54 +0200

    Autodifferenciation works really well. Some successful tests added.

--- src/qlib/numerical/autodiff.py
+++ src/qlib/numerical/autodiff.py
@@ -1,95 +1,67 @@
 from typing import Self
 
 import numpy as np
+import xarray as xa
+
+
+def make_gradient(grad: float, id: str | list):
+    return xa.DataArray([grad], coords={"a": list(id)})
 
 
 class Variable:
-    def __init__(self, f: float | np.ndarray, df: np.ndarray | None = None):
-        self.f = np.array(f)
-        self.df = df if df is not None else np.ones_like(f)
+    def __init__(self, id: str, f: float = 1.0, grad: float = 1.0):
+        self.id = [id] if isinstance(id, str) else id
+        self.f = f
+        self.grad = make_gradient(grad, id) if isinstance(grad, float) else grad
 
-    def __pow__(self, a: float) -> Self:
-        x = self.f**a
-        dx = self.df * a * self.f ** (a - 1)
-        return Variable(x, dx)
+    def __add__(self, other: Self):
+        f = self.f + other.f
+        grad_aligned, other_grad_ligned = xa.align(
+            self.grad, other.grad, join="outer", fill_value=0.0
+        )
+        df = grad_aligned + other_grad_ligned
+        id = df.coords["a"].values.tolist()
+        return Variable(id, f, df)
 
-    def __add__(self, other: Self | float | np.ndarray) -> Self:
-        match other:
-            case Variable(f=y, df=dy):
-                x = self.f + y
-                dx = self.df + dy
-            case _:
-                x = self.f + other
-                dx = self.df
-        return Variable(x, dx)
+    def __mul__(self, other: Self):
+        f = self.f * other.f
+        grad_aligned, other_grad_ligned = xa.align(
+            self.grad, other.grad, join="outer", fill_value=0.0
+        )
+        grad = grad_aligned * other.f + other_grad_ligned * self.f
+        id = grad.coords["a"].values.tolist()
+        return Variable(id, f, grad)
 
-    def __radd__(self, other: Self) -> Self:
-        return self.__add__(other)
+    def exp(self):
+        f = np.exp(self.f)
+        grad = np.exp(self.f) * self.grad
+        return Variable(self.id, f, grad)
 
-    def __sub__(self, other: Self) -> Self:
-        return self.__add__(-1 * other)
-
-    def __mul__(self, other: Self | float | np.ndarray) -> Self:
-        match other:
-            case Variable(f=y, df=dy):
-                x = self.f * y
-                dx = self.df * y + self.f * dy
-            case _:
-                x = self.f * other
-                dx = self.df * other
-        return Variable(x, dx)
-
-    def __rmul__(self, other: Self):
-        return self.__mul__(other)
-
-    def __truediv__(self, other: Self):
-        match other:
-            case Variable(f=g, df=dg):
-                x = self.f / g
-                dx = self.df / g - self.f / (g * g) * dg
-            case _:
-                return self * (1 / other)
-        return Variable(x, dx)
-
-    def __rtruediv__(self, other: float | np.ndarray):
-        match other:
-            case Variable(_, _):
-                return other / self
-            case _:
-                x = other / self.f
-                dx = -other / (self.f * self.f)
-                return Variable(x, dx)
-
-    def exp(self) -> Self:
-        x = np.exp(self.f)
-        dx = np.exp(self.f) * self.df
-        return Variable(x, dx)
+    def sin(self):
+        f = np.sin(self.f)
+        grad = np.cos(self.f) * self.grad
+        return Variable(self.id, f, grad)
 
     def __repr__(self):
-        return f"Variable(f={self.f}, df={self.df})"
+        return f"Var[{self.id}](f={self.f}, grad={self.grad.data})"
 
-    def sin(self) -> Self:
-        x = np.sin(self.f)
-        dx = np.cos(self.f) * self.df
-        return Variable(x, dx)
+    def __pow__(self, n: float):
+        f = self.f**n
+        grad = n * self.f ** (n - 1) * self.grad
+        return Variable(self.id, f, grad)
 
-    def log(self) -> Self:
-        x = np.log(self.f)
-        dx = self.df * (1 / self.f)
-        return Variable(x, dx)
+    def log(self):
+        f = np.log(self.f)
+        grad = self.grad / self.f
+        return Variable(self.id, f, grad)
 
 
 def main():
-    # x = Variable(np.array([3])[:, np.newaxis, np.newaxis])
-    # y = Variable(np.array([-1])[np.newaxis, :, np.newaxis])
-    # z = Variable(np.array([2])[np.newaxis, np.newaxis, :])
-    # f = x * y * (z * y).sin()
-    # print(f)
-    x = Variable([1, 2, 3])
-    a = Variable(1)
-    z = x.exp() ** 2
-    y = x + a / z
-    print(y)
+    x = Variable("x", f=3)
+    y = Variable("y", f=-1)
+    z = Variable("z", f=2)
+    f = (((x * y) * (y * z).sin()) ** 2).log()
+    print(f)
 
 
 if __name__ == "__main__":
--- tests/test_autodiff.py
+++ tests/test_autodiff.py
@@ -4,17 +4,30 @@ from qlib.numerical.autodiff import Variable
 
 def test_derivatives():
     val = 3.5
-    x = Variable(val)
-    assert x.df == 1
+    x = Variable("x", f=val)
+    assert x.grad == 1
 
     y = x**2
-    assert y.df == 3.5 * 2
+    np.testing.assert_almost_equal(y.grad.data, [3.5 * 2])
 
     z = x.exp().sin()
-    assert z.df == np.cos(np.exp(val)) * np.exp(val)
+    np.testing.assert_almost_equal(z.grad.data, np.cos(np.exp(val)) * np.exp(val))
 
 
-# def test_non_trivial_derivatives():
-#     x = Variable(5)
-#     y = Variable(3)
-#     z = x * y
+def test_non_trivial_derivatives_1():
+    x = Variable("x", f=3)
+    y = Variable("y", f=-1)
+    z = Variable("z", f=2)
+    f = (((x * y) * (y * z).sin()) ** 2).log()
+    np.testing.assert_almost_equal(np.round(f.grad.data, 3), [0.667, -0.169, -0.915])
+
+
+def test_non_trivial_derivatives_exactly():
+    x = Variable("x", f=3)
+    y = Variable("y", f=-1)
+    z = Variable("z", f=2)
+    f = (x * y) * (y * z).sin()
+    np.testing.assert_equal(
+        np.round(f.grad.data, 3),
+        [np.sin(2), -3 * (np.sin(2) + 2 * np.cos(2)), 3 * np.cos(2)],
+    )

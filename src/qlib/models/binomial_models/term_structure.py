47bf74238ac808a3a21712183486e6857857d260
Philippe LE <le.philippe.duc.tai@gmail.com>
2024-07-28 23:01:54 +0200

    Assignment done.

--- src/qlib/models/binomial_models/term_structure.py
+++ src/qlib/models/binomial_models/term_structure.py
@@ -1,4 +1,8 @@
 import numpy as np
+from numba import njit
+from qlib.models.binomial_models.american_options import (
+    AmericanCallOption,
+)
 from qlib.models.binomial_models.binomial_trees import BinomialTree, FlatForward
 from qlib.models.binomial_models.european_options import (
     EuropeanCallOption,
@@ -81,6 +85,57 @@ class ShortRateLatticeCustom:
         return self._lattice
 
 
+@njit
+def arrow_debreu_lattice(term_structure_lattice: np.ndarray, q: float):
+    sr = term_structure_lattice
+    lattice = np.zeros_like(sr)
+    lattice[0, 0] = 1
+    n = len(sr)
+    for k in range(0, n - 1):
+        lattice[k + 1, k + 1] = q * lattice[k, k] / (1 + sr[k, k])
+        lattice[0, k + 1] = (1 - q) * lattice[0, k] / (1 + sr[0, k])
+        for s in range(1, k + 1):
+            left = lattice[s - 1, k] * q / (1 + sr[s - 1, k])
+            right = lattice[s, k] * (1 - q) / (1 + sr[s, k])
+            lattice[s, k + 1] = left + right
+    return lattice
+
+
+class ForwardSwap:
+    def __init__(
+        self,
+        term_structure: BinomialTree,
+        swap_rate: float,
+        notional: float,
+        start: int,
+        end: int,
+        payer: bool = True,
+    ):
+        self.term_structure = term_structure
+        self.swap_rate = swap_rate
+        self.orient = 1 if payer else -1
+        self.start = start
+        self.end = end
+        self.notional = notional
+
+    def payoff(self, r):
+        return self.orient * (r - self.swap_rate) / (1 + r)
+
+    def payoff_values(self):
+        short_rates_lattice = self.term_structure.lattice()
+        return np.triu(self.payoff(short_rates_lattice))
+
+    def npv(self):
+        a, b = self.start, self.end
+        ts = self.term_structure.lattice()
+        q = self.term_structure.q
+        ad = arrow_debreu_lattice(ts, q)[:, a:b]
+        values = self.payoff_values()[:, a:b]
+        print(ad.round(4))
+        print(values.round(4))
+        return self.notional * (ad * values).sum()
+
+
 def main():
     r0 = 0.06
     u, d = 1.25, 0.9
@@ -101,12 +156,61 @@ def main():
     caplet = CapletOption(5, term_structure, term_structure, K_strike=0.02)
     print(caplet.lattice().round(6))
 
+    # Swaps derivatives
+
     # ts = BinomialTree(0.02, 3, u=1.15, d=0.95)
     # print(ts.lattice())
+    term_structure = BinomialTree(r0, 7, dt=1, u=u, d=d)
+    ad = arrow_debreu_lattice(term_structure.lattice(), 0.5)
+    print(ad.round(4))
 
+    # ZCB(0, 4) from arrow debreu securities
+    print(ad[:, 1:4].sum() * 100)
+
+    # Forward-start swap :
+    # begins at t = 1, ends at t = 3
+    # notional : $1M
+    # fixed rate : 7%
+    # in arrears, rate seen at t = i - 1 to compute the payment at t = i
+
+    term_structure = BinomialTree(r0, 3, dt=1, u=u, d=d)
+    fsw = ForwardSwap(term_structure, 0.07, 1_000_000, 1, 3, payer=False)
+    print(fsw.npv())
     # coupon_bond = ForwardCouponBond([0, 0, 0, 100], term_structure)
     # print(coupon_bond.lattice().round(4))
 
+    ts = ShortRateLatticeCustom(
+        [
+            [0.06, 0.075, 0.0938, 0.1172],
+            [0, 0.054, 0.0675, 0.0844],
+            [0, 0, 0.0486, 0.0608],
+            [0, 0, 0, 0.0437],
+        ]
+    )
+    ad = arrow_debreu_lattice(ts.lattice(), 0.5)
+    print(ad.round(4))
+    fsw = ForwardSwap(ts, 0.07, 100_000, 1, 3, payer=False)
+    print(fsw.npv().round(4))
+
+
+def assignment():
+    ts = BinomialTree(0.05, 10, u=1.1, d=0.9)
+    zcb = ZeroCouponBond(10, ts)
+    print(np.round(zcb.npv() * 100, 2))
+
+    forward_zcb = EuropeanOption(4, zcb, ts)
+    q2 = forward_price_bond(
+        4, coupon_array=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 100], term_structure=ts
+    )
+    print(np.round(q2, 2))
+    fwd = ForwardCouponBond([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 100], ts)
+    futures = FuturesOption(4, fwd)
+    print(futures.npv())
+
+    zcb_call = AmericanCallOption(6, zcb, ts, 0.8)
+    print(np.round(zcb_call.npv() * 100, 2))
+
 
 if __name__ == "__main__":
-    main()
+    # main()
+    assignment()
